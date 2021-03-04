2eb96ce37f6348f0f02102a24342450ba2070862
gilbert-noriega-ii <gilbert.noriega.ii@gmail.com>
2021-03-03 22:12:56 -0600

    initial commit to github

---
+++ summarizing_data.py
@@ -0,0 +1,5 @@
+#Load the 'per_capita_income.csv' file into income. No additional arguments other than the file name are needed. (Note that this is a csv file.)
+#Inspect the column names and data types with .info().
+#Using .sort_values(), sort (in descending order) the income DataFrame by the column which contains the income information.
+#Display the first five rows of income using .head() and the last five rows using .tail().
+
